71b7cefdd4a12e8080f9de8d538ebc241d1f8114
EoinKyne <eoin_kyne@yahoo.com>
2024-01-11 17:15:27 +0000

    Updated tests

--- src/test/java/org/studentmanager/StudentControllerTest.java
+++ src/test/java/org/studentmanager/StudentControllerTest.java
@@ -22,25 +22,23 @@ public class StudentControllerTest {
     @Mock
     private StudentRepository studentRepository;
     @Mock
-    private StudentGradeLabel studentGradeLabel;
+    private StudentService studentService;
     @Mock
     private Student student = new Student("John Doe", 3.5);
 
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
-        studentController = new StudentController(studentRepository, studentGradeLabel);
+        studentController = new StudentController(studentRepository, studentService);
     }
 
     @Test
     public void addStudent() {
         Student s = new Student(1L, "John Doe", 3.3);
-        given(studentGradeLabel.calculateGradePointAverageLabel(anyDouble())).willReturn("C");
-        given(studentRepository.addStudentGradePointAverageLabel(s)).willReturn(s);
-        given(studentRepository.generateStudentId()).willReturn(1L);
-        given(studentRepository.saveStudent(s)).willReturn(true);
+        given(studentService.addNewStudent(s)).willReturn(s);
         Student newStudent = studentController.addStudent(s);
         Assertions.assertEquals(s.toString(), newStudent.toString());
+        Assertions.assertEquals(3.3, newStudent.getGradePointAverage());
     }
 
     @Test
@@ -52,67 +50,36 @@ public class StudentControllerTest {
 
     @Test
     public void testGetStudentById(){
-        given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
-        System.out.println(student.hashCode());
+        //given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
+        given(studentService.getStudentById(anyLong())).willReturn(student);
+        //System.out.println(student.hashCode());
         Student returnedStudent = studentController.getStudentById(1L);
-        System.out.println(returnedStudent.hashCode());
+        //System.out.println(returnedStudent.hashCode());
         Assertions.assertEquals(returnedStudent, student);
     }
 
     @Test
     public void testAmendGradePointAverageByStudent(){
-        Student amendedStudent = new Student(1L, "Jane Doe", 4.0);
 
-        given(studentRepository.addStudentGradePointAverageLabel(amendedStudent)).willReturn(amendedStudent);
-        given(studentRepository.amendGradePointAverage(anyLong(), anyDouble())).willReturn(amendedStudent);
-        given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
+        Student studentGPA = new Student(1L, "Jane Doe", 4.0);
+        given(studentService.amendGPAForStudentId(anyLong(), anyDouble())).willReturn(studentGPA);
+        //Student amendedStudentGPA = studentController.amendGradePointAverageByStudent(1L, 3.5);
 
-        Student amendedStudentGPA = studentController.amendGradePointAverageByStudent(1L, 4.0);
-
-        Assertions.assertEquals(amendedStudentGPA, amendedStudent);
-        Assertions.assertEquals(4.0, amendedStudentGPA.getGradePointAverage());
-    }
-
-    @Test
-    public void testStudentNotFoundException(){
-        StudentNotFoundException studentNotFoundException = Assertions.assertThrows(StudentNotFoundException.class, () -> {
-            studentController.getStudentById(1L);
-        }, "StudentNotFoundException exception was expected");
-        Assertions.assertEquals("StudentId is not found 1", studentNotFoundException.getMessage());
+        //Assertions.assertEquals(amendedStudentGPA, s);
+        Assertions.assertEquals(4.0, studentGPA.getGradePointAverage());
     }
 
     @Test
     public void testFinalizeStudentGradePointAverage(){
         Student finalStudent = new Student(1L, "Jane Dow", 2.0, "C", true);
 
-        given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
-        given(studentRepository.finalizeGradePointAverage(anyLong())).willReturn(finalStudent);
+        given(studentService.finalizeStudentGPA(anyLong())).willReturn(finalStudent);
+        //given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
+        //given(studentRepository.finalizeGradePointAverage(anyLong())).willReturn(finalStudent);
 
         Student finalizedStudent = studentController.finalizeGradePointAverageByStudent(1L);
 
         Assertions.assertEquals(true, finalizedStudent.isGradePointAverageChecked());
         Assertions.assertEquals("C", finalizedStudent.getGradePointAverageLabel());
     }
-
-    @Test
-    public void testStudentGpaIsOutOfBoundsException(){
-        given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(student);
-        StudentGradePointAverageOutOfBoundsException studentGradePointAverageOutOfBoundsException =
-                Assertions.assertThrows(StudentGradePointAverageOutOfBoundsException.class, () -> {
-            studentController.amendGradePointAverageByStudent(1L, 5.5);
-        }, "StudentGradePointAverageOutOfBoundsException exception was expected");
-        Assertions.assertEquals("GPA should be between 0.0 to 4.0. Provided GPA 5.5 is out of bounds.",
-                studentGradePointAverageOutOfBoundsException.getMessage());
-    }
-
-    @Test
-    public void testStudentAmendException(){
-        Student finalStudent = new Student(1L, "Jane Dow", 2.0, "C", true);
-        given(studentRepository.retrieveStudentFromRepo(anyLong())).willReturn(finalStudent);
-        StudentAmendException studentAmendException = Assertions.assertThrows(StudentAmendException.class, () -> {
-            studentController.amendGradePointAverageByStudent(1L, 5.5);
-        }, "StudentAmendException exception was expected");
-        Assertions.assertEquals("Student GPA is finalized and cannot be amended for student studentId 1", studentAmendException.getMessage());
-    }
-
 }
--- src/test/java/org/studentmanager/StudentIntegrationTests.java
+++ src/test/java/org/studentmanager/StudentIntegrationTests.java
@@ -8,37 +8,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 
 public class StudentIntegrationTests {
 
-    private StudentController studentController;
+    private StudentController studentController = new StudentController();
     private Map<Long, Student> repository = new HashMap<>();
     private StudentRepository studentRepository = new StudentRepository(repository);
+    //private StudentGradeLabel studentGradeLabel;
+    private StudentService studentService = new StudentService();
     private Student student;
 
     @BeforeEach
     public void setUp(){
-        studentController = new StudentController(studentRepository);
+        studentController = new StudentController(studentRepository, studentService);
     }
 
     @Test
     public void addStudentTest(){
-        student = new Student("Jane Doe", 2.5);
+        student = new Student("Jane Doe", 1.5);
         student = studentController.addStudent(student);
         Assertions.assertEquals(1L, student.getStudentId());
     }
 
     @Test
     public void amendStudentTest(){
-        student = new Student( "Jane Doe", 2.3);
+        student = new Student( "Jane Doe", 2.5 );
         student = studentController.addStudent(student);
         Student amendedStudent = studentController.amendGradePointAverageByStudent(student.getStudentId(), 3.3);
         Assertions.assertEquals(3.3, amendedStudent.getGradePointAverage());
+        Assertions.assertEquals("B+", amendedStudent.getGradePointAverageLabel());
     }
 
     @Test
     public void getStudentByIdTest(){
-        student = new Student( "Jane Doe", 2.3);
+        student = new Student( 1L,"Jane Doe", 2.5);
         student = studentController.addStudent(student);
         Student returnedStudent = studentController.getStudentById(1L);
         Assertions.assertEquals(1, returnedStudent.getStudentId());
@@ -46,23 +52,85 @@ public class StudentIntegrationTests {
 
     @Test
     public void getAllStudentsSortedByGPA(){
-        Student studentLow = new Student("Tom Doe", 1.2);
-        Student studentHigh = new Student("Tina Doe", 1.3);
+        Student studentLow = new Student("Jane Doe",2.5);
+        Student studentHigh = new Student("John Doe", 2.5);
 
         studentLow = studentController.addStudent(studentLow);
         studentHigh = studentController.addStudent(studentHigh);
 
-        List<Student> studentList = studentController.getAllStudents();
+        List<Student> studentList = studentService.getAllStudents();
         Assertions.assertEquals(2, studentList.size());
-        Assertions.assertEquals(2, studentList.get(0).getStudentId());
+        Assertions.assertEquals(2, studentList.get(1).getStudentId());
     }
 
     @Test
     public void finalizeStudentGradePointAverageTest(){
-        student = new Student( "Jane Doe", 2.3);
+        student = new Student( "Jane Doe", 2.5);
         student = studentController.addStudent(student);
 
         Student finalizedStudent = studentController.finalizeGradePointAverageByStudent(1L);
         Assertions.assertEquals(true, finalizedStudent.isGradePointAverageChecked());
+    }
+
+    @Test
+    public void addNewStudentWithoutName(){
+        student = new Student( "", 2.5);
+        StudentNameIsEmptyException studentNameIsEmptyException = Assertions.assertThrows(StudentNameIsEmptyException.class, () -> {
+            studentController.addStudent(student);
+        }, "StudentNameIsEmptyException exception was expected");
+        Assertions.assertEquals("Cannot add new student without a full name", studentNameIsEmptyException.getMessage());
+    }
+
+    @Test
+    public void addNewStudentWithGPAOutOfRange(){
+        student = new Student( "Jane Doe", 4.5);
+        StudentGradePointAverageOutOfBoundsException studentGradePointAverageOutOfBoundsException = Assertions.assertThrows(StudentGradePointAverageOutOfBoundsException.class, () -> {
+            studentController.addStudent(student);
+        }, "StudentGradePointAverageOutOfBoundsException exception was expected" );
+        Assertions.assertEquals("GPA should be between 0.0 to 4.0. Provided GPA 4.5 is out of bounds.", studentGradePointAverageOutOfBoundsException.getMessage());
+    }
+
+    @Test
+    public void updateStudentWithGPAOutOfRange(){
+        student = new Student( "Jane Doe", 2.5);
+        student = studentController.addStudent(student);
+        StudentGradePointAverageOutOfBoundsException studentGradePointAverageOutOfBoundsException = Assertions.assertThrows(StudentGradePointAverageOutOfBoundsException.class, () -> {
+            studentController.amendGradePointAverageByStudent(1L, 4.5);
+        }, "StudentGradePointAverageOutOfBoundsException exception was expected" );
+        Assertions.assertEquals("GPA should be between 0.0 to 4.0. Provided GPA 4.5 is out of bounds.", studentGradePointAverageOutOfBoundsException.getMessage());
+    }
+
+    @Test
+    public void studentNotFoundWhenAmendingGPA(){
+        StudentNotFoundException studentNotFoundException = Assertions.assertThrows(StudentNotFoundException.class, () -> {
+            studentController.amendGradePointAverageByStudent(1L, 2.7);
+        }, "StudentNotFoundException exception was expected");
+        Assertions.assertEquals("StudentId is not found 1", studentNotFoundException.getMessage());
+    }
+
+    @Test
+    public void studentNotFoundWhenFinalizingGPA(){
+        StudentNotFoundException studentNotFoundException = Assertions.assertThrows(StudentNotFoundException.class, () -> {
+            studentController.finalizeGradePointAverageByStudent(1L);
+        }, "StudentNotFoundException exception was expected");
+        Assertions.assertEquals("StudentId is not found 1", studentNotFoundException.getMessage());
+    }
+
+    @Test
+    public void studentNotFoundWhenGettingStudentById(){
+        StudentNotFoundException studentNotFoundException = Assertions.assertThrows(StudentNotFoundException.class, () -> {
+            studentController.getStudentById(1L);
+        }, "StudentNotFoundException exception was expected");
+        Assertions.assertEquals("StudentId is not found 1", studentNotFoundException.getMessage());
+    }
+
+    @Test
+    public void studentAmendExceptionWhenStudentIsMarkedFinalized(){
+        student = new Student("Jane Doe", 3.9, "A-", true );
+        student = studentController.addStudent(student);
+        StudentAmendException studentAmendException = Assertions.assertThrows(StudentAmendException.class, () -> {
+            studentController.amendGradePointAverageByStudent(1L, 3.8);
+        }, "StudentAmendException exception was expected");
+        Assertions.assertEquals("Student GPA is finalized and cannot be amended for student studentId 1", studentAmendException.getMessage());
     }
 }
